c82310ddd5eca1940fda2775b521d44604de32f0
kenetcode <k2ken2424@gmail.com>
2023-01-09 22:56:30 -0600

    Add files

---
+++ src/Doctor.java
@@ -0,0 +1,22 @@
+public class Doctor {
+    static int id = 0 ; //Autoincrement
+    String name;
+    String speciality;
+
+    Doctor(){
+        System.out.println("Construyendo el objeto Doctor");
+        id++;
+    }
+    Doctor(String name){
+        System.out.println("El nombre del Doctor asignado es: " + name);
+    }
+
+    //Comportamientos
+    public void showName(){
+        System.out.println(name);
+    }
+    public void showId(){
+        System.out.println("ID Doctor: " + id);
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Doctor myDoctor = new Doctor();
+        myDoctor.name = "Kenet Ortiz";
+        myDoctor.showName();
+        myDoctor.showId();
+
+        Doctor myDoctorKenet = new Doctor();
+        myDoctor.showId();
+
+
+
+
+
+    }
+}
